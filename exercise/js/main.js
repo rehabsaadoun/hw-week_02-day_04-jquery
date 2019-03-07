274b5ec6a6ed47c2ba2fcf9e38790d2663dcabae
rehabsaadoun <YOUR GITHUB EMAIL>
2019-03-07 07:36:26 +0300

    rehab

--- exercise/js/main.js
+++ exercise/js/main.js
@@ -9,28 +9,28 @@
 //SECTION 1: REPLACE THE SECTIONS THAT SAY REPLACE ME WITH THE CORRECT CODE.
 
 /**
- * Question 1:
- * When, button 1 is clicked, select all paragraphs on the page with jQuery, and changed the font color to blue.
- * We've started you off, ONLY replace everything instance of "replaceMe" with correct code.
+ *Question 1:
+ *When, button 1 is clicked, select all paragraphs on the page with jQuery, and changed the font color to blue.
+ *We've started you off, ONLY replace everything instance of "replaceMe" with correct code.
  */
 
 function question1() {
-	$("replaceMe").css("replaceMe", "replaceMe");
+	$("p").css("color", "blue");
 }
 
 $("#button1").on('click', question1);
 
-/**
+/** 
  * Question 2:
  * When button 1 is clicked, select all <h2> elements on this page, and change the text to say your name.
  * Uncomment the code below and solve the problem
- */
+ 
+*/
+	function question2(){
+		$("h2").html("Robe");
+	 }
 
-	// function question2(){
-	// 	$("replaceMe").replaceMe("replaceMe");
-	// }
-
-	// $("#button1").on('click', question2);
+	 $("#button1").on('click', question2);
 
 /**
  * Question 3:
@@ -39,11 +39,11 @@ $("#button1").on('click', question1);
  * Uncomment the code below and solve the problem
  */
 
-	// function question3(){
-	// 	$("replaceMe").html("replaceMe");
-	// }
+	 function question3(){
+		$("li.falseFact").html("li.True Fact");
+ }
 
-	// $("#button1").on('click', question3);
+	 $("#button1").on('click', question3);
 
 
 //SECTION 2: Now you are on your own. Write the necessary code to accomplish the following.
@@ -52,14 +52,22 @@ $("#button1").on('click', question1);
  * Question 4:
  * When button 2 is clicked, change the background color of the whole page to "pink"
  */
+$('#button2').click(question4);
 
+  	function question4() {
+ 		$('body').css('background-color', 'pink');
+ 	}
 
 
 /**
  * Question 5:
  * When button 2 is clicked, change the color of all h2's to "green"
  */
+$('#button2').click(question5)
 
+  	function question5() {
+ 		$('h2').css('color', 'green');
+ 	}
 
 
 /**
@@ -67,7 +75,11 @@ $("#button1").on('click', question1);
  * When button 2 is clicked, change the html content of all "blockquotes" to be "<span>no quote</span>"
  */
 
+$('#button2').click(question6);
 
+  	function question6() {
+ 		$('blockquote').html('<span>no quote</span>');
+ 	}
 //SECTION 3: This section is more difficult. Try your hand at the next 3 questions.
 
 /**
@@ -75,25 +87,38 @@ $("#button1").on('click', question1);
  * When button 3 is clicked, change the text in the header's h1 tag to read "jQuery Ninja"
  */
 
+$('#button3').click(question7);
 
-/**
- * Question 8:
+  	function question7() {
+		 $('header h1').html("jQuery Ninja");
+	  }
+/** * Question 8:
  * Have the following code execute when button 3 is clicked.
  * $(".city").attr("src", "http://lorempixel.com/g/500/400/food");
  */
+$('#button3').click(question8);
 
+  	function question8() {
+		 $(".city").attr("src", "http://lorempixel.com/g/500/400/food");
+	  }
 
 /**
  * Question 9:
  * When button 3 is clicked, select all paragraphs on the page and change the color to blue and font to Georgia.
  */
+$('#button3').click(question9);
 
-
+  	function question9() {
+ 		$('p').css('color:blue',font-family,"Georgia");
+	  }
 /**
  * Question 10:
  * Try to do Question 9 but with only one jQuery call. $("selector").css({attribute: "value", attribute: "value"})
  */
+$('#button3').click(question9);
 
-
+  	function question10() {
+ 		$('p').css('color:blue',font-family,"Georgia");
+	  }
 
 
